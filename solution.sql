50db30a942bce4e1a6b6f69e8e0ed8208f35c016
Pisajm <pisajm@farmingdale.edu>
2026-04-26 20:28:48 -0400

    QueryArtistPaymentssql
    Signed-off-by: Pisajm <pisajm@farmingdale.edu>

--- solution.sql
+++ solution.sql
@@ -1,1 +1,11 @@
--- Write your SQL query below:
+SELECT
+    Artists.ArtistName,
+    Payments.PeriodStart,
+    Payments.PeriodEnd,
+    Payments.AmountPaid,
+    Payments.PaymentDate
+FROM
+    Artists
+    INNER JOIN Payments ON Artists.ArtistID = Payments.ArtistID
+ORDER BY
+    Payments.AmountPaid DESC;-- Write your SQL query below:
