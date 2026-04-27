f7031aa6098f26ba7f9b587927b83b5f8901bcea
Pisajm <pisajm@farmingdale.edu>
2026-04-26 20:52:37 -0400

    Update solution.sql
    Signed-off-by: Pisajm <pisajm@farmingdale.edu>

--- solution.sql
+++ solution.sql
@@ -1,4 +1,9 @@
 -- Write your SQL query below:
-SELECT
-    Songs.SongTitle,
-    Count(Streams.StreamID) AS TotalStreams
+SELECT Artists.ArtistName,
+COUNT(Streams.StreamID) AS TotalStreams
+FROM Artists
+JOIN Albums ON Artists.ArtistID = Albums.ArtistID
+JOIN Songs ON Albums.AlbumID = Songs.AlbumID
+JOIN Streams ON Songs.SongID = Streams.SongID
+GROUP BY Artists.ArtistName
+ORDER BY TotalStreams DESC;
